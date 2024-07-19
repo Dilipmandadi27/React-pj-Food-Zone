d6ebcdb47308f482913f7d2e940992b579912b58
Dilipmandadi27 <148129250+Dilipmandadi27@users.noreply.github.com>
2024-07-19 19:57:05 +0530

    Add Footer component

---
+++ Food-Zone/src/assets/Components/Footer.jsx
@@ -0,0 +1,17 @@
+
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+        <div className="indicator">
+      <h3>Made with ❤️_@Dilipaaa</h3>
+      </div>
+      <div className="para">
+      <p>Contact Us <br />
+         @ 270619982000</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
